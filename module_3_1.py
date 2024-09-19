f8f16e09b46c31353e9956e26130525fafa86102
zezikvl <zezik.vl@gmail.com>
2024-09-19 19:46:55 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [i.upper() for i in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
